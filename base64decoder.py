8be4b6e65929f70618df6caf3e485ed5880854ae
Fluturel Adrian <fluturel.adrian@gmail.com>
2018-09-15 02:59:14 +0300

    Added main function and..
    .. more sophisticated command line arguments. Still, --decode doesn't do anything now

--- base64decoder.py
+++ base64decoder.py
@@ -1,11 +1,8 @@
+import argparse
 import sys
 import textwrap as tw
 
-ascii_dict = {i : chr(i) for i in range(0,128)}
-base64_dict = {}
-output_string = ""
-
-def build_base64_dict():
+def build_base64_dict(base64_dict):
 	for i in range(0, 63):
 		if i in range(0, 26):
 			base64_dict[i] = chr(i+65)
@@ -26,22 +23,36 @@ def convert_binary(number):
 	binary_number = str(bin(number))[2:]
 	return "0"*(6-len(binary_number)) + binary_number
 
-build_base64_dict()
+def main(args):
+	ascii_dict = {i : chr(i) for i in range(0,128)}
+	base64_dict = {}
+	output_string = ""
 
-string_to_decode = ""
-if len(sys.argv) == 1:
-	string_to_decode = input("Input the string to decode: ")
-else:
-	string_to_decode = str(sys.argv[1])
-if len(string_to_decode) % 4 != 0:
-	print("Invalid input, can't decode string")
-	exit
+	build_base64_dict(base64_dict)
 
-string_to_decode = tw.wrap(string_to_decode, 4)
+	string_to_decode = args.message
+	decode_flag = args.decode
 
-for group in string_to_decode:
-	indexes = list(map(lambda x : get_key_from_dict(x, base64_dict), group))
-	binary_group = ''.join(list(map(convert_binary, indexes)))
-	for ascii_char in tw.wrap(binary_group, 8):
-		output_string += ascii_dict[int(ascii_char, 2)]
-print(output_string)
+	if len(string_to_decode) == 0:
+		string_to_decode = input("Input the string to decode: ")
+
+	if len(string_to_decode) % 4 != 0:
+		print("Invalid input, can't decode string")
+		exit
+
+	string_to_decode = tw.wrap(string_to_decode, 4)
+
+	for group in string_to_decode:
+		indexes = list(map(lambda x : get_key_from_dict(x, base64_dict), group))
+		binary_group = ''.join(list(map(convert_binary, indexes)))
+		for ascii_char in tw.wrap(binary_group, 8):
+			output_string += ascii_dict[int(ascii_char, 2)]
+	print(output_string)
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Utility for base64 encoded text")
+	parser.add_argument("-d", "--decode", action="store_true", help="decode text")
+	parser.add_argument("-m", "--message", default="", metavar="M", help="message to be encode/decoded")
+
+	args = parser.parse_args()
+	main(args)
